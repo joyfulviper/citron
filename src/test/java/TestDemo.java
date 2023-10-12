3004d3aa78959b0562899d1498e19d25e282319c
joyfulviper <kimsk3113@naver.com>
2023-10-13 02:53:51 +0900

    test : 브랜치

--- src/test/java/TestDemo.java
+++ src/test/java/TestDemo.java
@@ -1,29 +1,35 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class TestDemo {
+class TestDemo {
 
     @Test
     @DisplayName("테스트")
-    public void test1() {
+    void test1() {
         System.out.println();
     }
 
     @Test
     @DisplayName("테스트")
-    public void test2() {
+    void test2() {
         System.out.println();
     }
 
     @Test
     @DisplayName("테스트")
-    public void test3() {
+    void test3() {
         System.out.println();
     }
 
     @Test
     @DisplayName("테스트")
-    public void test4() {
+    void test4() {
+        System.out.println();
+    }
+
+    @Test
+    @DisplayName("테스트")
+    void test5() {
         System.out.println();
     }
 }
